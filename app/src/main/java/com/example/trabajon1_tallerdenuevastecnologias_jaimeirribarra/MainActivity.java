9eceffed71acc3e5dbdcd99cb7044039476de403
TrabajoN1 <jaime.irribarra@virginiogomez.cl>
2021-10-18 20:13:48 -0300

    Version 2.0

--- app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity.java
+++ app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.trabajon1_tallerdenuevastecnologias_jaimeirribarra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button Botoncalcular;
+    private EditText Textnombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Textnombre = (EditText) findViewById(R.id.TextNombre);
+        Botoncalcular =(Button) findViewById(R.id.buttonCalcular);
+        Intent intentCalc=new Intent(this,MainActivity2.class);
+        Botoncalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentCalc.putExtra("nombre",Textnombre.getText().toString());
+                startActivity(intentCalc);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity2.java
+++ app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity2.java
@@ -2,13 +2,43 @@ package com.example.trabajon1_tallerdenuevastecnologias_jaimeirribarra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button BotonVolver;
+    private String stNombre;
+    private TextView TextLabelNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextLabelNombre=(TextView) findViewById(R.id.TextLabelNombre);
+        Context context=getApplicationContext();
+
+        Bundle intentV=this.getIntent().getExtras();
+        if (intentV!=null){
+            stNombre=intentV.getString("nombre");
+            Toast.makeText(context,"Su Nombre es: "+stNombre,Toast.LENGTH_SHORT).show();
+        }
+
+        TextLabelNombre.setText("Nombre del estudiante: "+ stNombre);
+
+        BotonVolver = (Button) findViewById(R.id.buttonVolver);
+        Intent intentVol=new Intent(this,MainActivity.class);
+
+        BotonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intentVol);
+            }
+        });
     }
 }
