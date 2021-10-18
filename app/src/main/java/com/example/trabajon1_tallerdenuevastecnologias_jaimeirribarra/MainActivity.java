434227ef6ad95ee1093b6d88f51317d09a1062c9
TrabajoN1 <jaime.irribarra@virginiogomez.cl>
2021-10-18 20:55:26 -0300

    Version 3.0 Text

--- app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity.java
+++ app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity.java
@@ -12,11 +12,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button Botoncalcular;
     private EditText Textnombre;
+    private EditText TextCarrera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextCarrera = (EditText) findViewById(R.id.TextCarrera);
         Textnombre = (EditText) findViewById(R.id.TextNombre);
         Botoncalcular =(Button) findViewById(R.id.buttonCalcular);
         Intent intentCalc=new Intent(this,MainActivity2.class);
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 intentCalc.putExtra("nombre",Textnombre.getText().toString());
+                intentCalc.putExtra("carrera",TextCarrera.getText().toString());
                 startActivity(intentCalc);
             }
         });
--- app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity2.java
+++ app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity2.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textservice.TextInfo;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,21 +16,26 @@ public class MainActivity2 extends AppCompatActivity {
     private Button BotonVolver;
     private String stNombre;
     private TextView TextLabelNombre;
+    private String stCarrera;
+    private TextView TextInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         TextLabelNombre=(TextView) findViewById(R.id.TextLabelNombre);
+        TextInfo=(TextView) findViewById(R.id.TextInfo);
         Context context=getApplicationContext();
 
         Bundle intentV=this.getIntent().getExtras();
         if (intentV!=null){
+            stCarrera=intentV.getString("carrera");
             stNombre=intentV.getString("nombre");
-            Toast.makeText(context,"Su Nombre es: "+stNombre,Toast.LENGTH_SHORT).show();
+            Toast.makeText(context,"Estudiante: "+stNombre + " de la carrera "+stCarrera+ " Su promedio es: ",Toast.LENGTH_SHORT).show();
         }
 
         TextLabelNombre.setText("Nombre del estudiante: "+ stNombre);
+        TextInfo.setText("Estudiante: "+stNombre + " de la carrera "+stCarrera+ " Su promedio es: ");
 
         BotonVolver = (Button) findViewById(R.id.buttonVolver);
         Intent intentVol=new Intent(this,MainActivity.class);
